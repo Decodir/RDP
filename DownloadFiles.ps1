e5aa9de8572e7b9678332b0c9536e6d9570befe4
Decrypter <78876406+Decodir@users.noreply.github.com>
2024-11-16 00:17:50 +0300

    Add files via upload

--- DownloadFiles.ps1
+++ DownloadFiles.ps1
@@ -1,4 +1,5 @@
 $files = @(
+    @{ Uri = "https://github.com/Decodir/RDP/raw/refs/heads/main/Explorer++.exe"; Path = "c:\Users\Administrator\Downloads\Explorer++.exe" },
     @{ Uri = "https://github.com/Decodir/RDP/raw/refs/heads/main/Bot.exe"; Path = "c:\Users\Administrator\Downloads\Bot.exe" },
     @{ Uri = "https://www.python.org/ftp/python/3.13.0/python-3.13.0-amd64.exe"; Path = "c:\Users\Administrator\Downloads\python-3.13.0-amd64.exe" },
     @{ Uri = "https://download.sublimetext.com/Sublime%20Text%20Build%203211%20Setup.exe"; Path = "c:\Users\Administrator\Downloads\Sublime%20Text%20Build%203211%20Setup.exe" },
